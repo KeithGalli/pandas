8832944cc45c6de83f05b4920f0da2feb5bdbf1a
Somoye Christopher Adedeji <61905983+chris-netizen@users.noreply.github.com>
2022-09-06 23:00:58 +0000

    Update analysis.py
    I added comments to the code, for new comers to better understand the written code.

--- analysis.py
+++ analysis.py
@@ -1,6 +1,10 @@
+#The pandas library was imported into the work space for us, as pd(for easy use)
 import pandas as pd
 
+#The pandas read_csv command helps to read csv files, 
+#There are other commands depending on the file type...
 poke = pd.read_csv('pokemon_data.csv')
 
+#This code checks the last 5 rows of the pandas dataframe.
 print(poke.tail(5))
 
